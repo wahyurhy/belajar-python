2c5e1da52df12d4474f68fb0e7eb4fb643109f82
wahyurhy <wahyurahayu82@gmail.com>
2024-10-24 23:21:00 +0700

    latihan 5 fix bugs

--- latihan-pertemuan-5.py
+++ latihan-pertemuan-5.py
@@ -1,3 +1,5 @@
+import format_rupiah
+
 borderLine = "-----------------------------------------------------------------------------"
 titleApp = "Grobak Fried Chicken"
 
@@ -17,19 +19,26 @@ jenisPotongYangDibeli = []
 banyakYangDibeli = []
 jumlahHarga = []
 
-banyakJenis = int(input('Banyak Jenis:'))
+isTrue = True
+while isTrue:
+    try:
+        banyakJenis = int(input('Banyak Jenis:'))
+        isTrue = False
+    except:
+        print('Hanya Boleh diinputkan dengan angka!')
+        isTrue = True
 
 for i in range(banyakJenis):
     print('{:<25} {:^10} {:>10}'.format('', titleApp.upper(), ''))
     print(borderLine)
     print('{:<10} {:^10} {:>10}'.format('Kode', 'Jenis Potongan', 'Harga'))
-    print('{:<10} {:^14} {:>10}'.format(' D', 'Dada', str(produk['D'])))
-    print('{:<10} {:^14} {:>10}'.format(' P', 'Paha', str(produk['P'])))
-    print('{:<10} {:^14} {:>10}'.format(' S', 'Sayap', str(produk['S'])))
+    print('{:<10} {:^14} {:>10}'.format(' D', str(namaProduk['D']), str(produk['D'])))
+    print('{:<10} {:^14} {:>10}'.format(' P', str(namaProduk['P']), str(produk['P'])))
+    print('{:<10} {:^14} {:>10}'.format(' S', str(namaProduk['S']), str(produk['S'])))
     print(borderLine + '\n\n')
     print(f'Banyak Jenis: {banyakJenis}')
     print(f'Jenis Ke-{i + 1}')
-    potonganYangDipilih = str(input('Kode Potongan [D/P/S]: '))
+    potonganYangDipilih = str(input('Kode Potongan [D/P/S]: ').capitalize())
     jumlahYangDibeli = int(input('Banyak Potong: '))
 
     jenisPotongYangDibeli.append(potonganYangDipilih)
@@ -41,13 +50,13 @@ print(borderLine)
 print('No. {:^2} Jenis Potong  {:^5}  Harga Satuan {:^5} Banyak Beli {:^5} Jumlah Harga'.format('','','',''))
 print(borderLine)
 for i in range(len(banyakYangDibeli)):
-    print('{} {:^20} {:^20} {:^17} {:<5} Rp{}'.format(i + 1, namaProduk[jenisPotongYangDibeli[i]], produk[jenisPotongYangDibeli[i]], banyakYangDibeli[i], '', jumlahHarga[i]))
+    print('{} {:^20} {:^20} {:^17} {:<5} {}'.format(i + 1, namaProduk[jenisPotongYangDibeli[i]], format_rupiah.format(produk[jenisPotongYangDibeli[i]]), banyakYangDibeli[i], '', format_rupiah.format(jumlahHarga[i])))
 
 print(borderLine)
 
-print('{:<55}Jumlah Bayar Rp{}'.format('', sum(jumlahHarga)))
+print('{:<55}Jumlah Bayar {}'.format('', format_rupiah.format(sum(jumlahHarga))))
 
 pajak = (10/100) * sum(jumlahHarga)
-totalBayar = sum(jumlahHarga) - pajak
-print('{:<55}Pajak 10%    Rp{}'.format('', pajak))
-print('{:<55}Total Bayar  Rp{}'.format('', totalBayar))
+totalBayar = sum(jumlahHarga) + pajak
+print('{:<55}Pajak 10%    {}'.format('', format_rupiah.format(pajak)))
+print('{:<55}Total Bayar  {}'.format('', format_rupiah.format(totalBayar)))
